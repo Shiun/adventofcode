5aeff100f80b78b3aa79550936779d8579a61b00
Shiun Hwang <shiun@labzero.com>
2023-12-21 09:56:35 -0800

    Advent of Code Day 16 part 1

---
+++ 2023/15/part1.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("../utils/utils");
+const readInput = () => {
+    const lines = (0, utils_1.inputLinesArray)("input.txt", false);
+    return lines;
+};
+const parseLine = (line) => {
+    return line.split(",");
+};
+const formulateValue = (line) => {
+    let value = 0;
+    for (let i = 0; i < line.length; i++) {
+        let char = line[i];
+        value += char.charCodeAt(0);
+        value *= 17;
+        value %= 256;
+    }
+    return value;
+};
+const readSequence = () => {
+    const input = readInput();
+    console.log('input', input);
+    let sequence = parseLine(input[0]);
+    console.log('sequence', sequence);
+    console.log('H ascii code', 'H'.charCodeAt(0));
+    console.log('formulateValue(H)', formulateValue('HASH'));
+    let values = sequence.map(s => formulateValue(s));
+    console.log('values', values);
+    let sum = values.reduce((a, v) => a + v, 0);
+    console.log('sum', sum);
+};
+readSequence();
---
+++ 2023/15/part1.ts
@@ -0,0 +1,38 @@
+import { inputLinesArray } from "../utils/utils";
+
+const readInput = () => {
+  const lines = inputLinesArray("input.txt", false);
+  return lines;
+};
+
+const parseLine = (line: string) => {
+  return line.split(",");
+};
+
+const formulateValue = (line: string) => {
+  let value = 0
+  for(let i = 0; i < line.length; i++) {
+    let char = line[i]
+    value += char.charCodeAt(0)
+    value *= 17
+    value %= 256
+  }
+  return value
+}
+
+const readSequence = () => {
+  const input = readInput()
+  console.log('input', input)
+  let sequence = parseLine(input[0])
+  console.log('sequence', sequence)
+
+  console.log('H ascii code', 'H'.charCodeAt(0))
+  console.log('formulateValue(H)', formulateValue('HASH'))
+
+  let values = sequence.map(s => formulateValue(s))
+  console.log('values', values)
+  let sum = values.reduce((a, v) => a + v, 0)
+  console.log('sum', sum)
+}
+
+readSequence()
