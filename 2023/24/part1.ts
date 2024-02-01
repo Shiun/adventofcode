546d027e995ba7b287eaeb983c7d2cf68ac17de3
Shiun Hwang <shiun@labzero.com>
2024-01-31 16:46:01 -0800

    Advent of Code 2023 Day 24 Part 1

---
+++ 2023/24/part1.ts
@@ -0,0 +1,106 @@
+import { inputLinesArray } from "../utils/utils";
+const { inspect } = require("util");
+
+type Hail = {
+  x: number;
+  y: number;
+  z: number;
+  vx: number;
+  vy: number;
+  vz: number;
+};
+
+const readInput = () => {
+  const lines = inputLinesArray("input.txt");
+  return lines;
+};
+
+const parseHail = (line: string): Hail => {
+  const regex =
+    /^(?<x>[-\d]+)[, ]+(?<y>[-\d]+)[, ]+(?<z>[-\d]+)[@ ]+(?<vx>[-\d]+)[, ]+(?<vy>[-\d]+)[, ]+(?<vz>[-\d]+)$/;
+  let result = line.match(regex);
+  return {
+    x: parseInt(result?.groups?.x!),
+    y: parseInt(result?.groups?.y!),
+    z: parseInt(result?.groups?.z!),
+    vx: parseInt(result?.groups?.vx!),
+    vy: parseInt(result?.groups?.vy!),
+    vz: parseInt(result?.groups?.z!),
+  };
+};
+
+const hasIntersection = (h1: Hail, h2: Hail, min: number, max: number) => {
+  let m1 = h1.vy/h1.vx
+  let m2 = h2.vy/h2.vx
+
+  // Parallel Lines
+  if (m1 === m2) {
+    return false
+  }
+
+  // y = m*x + b, where m = slope and b = y intercept
+  // b = y - m*x
+
+  let b1 = h1.y - m1 * h1.x
+  let b2 = h2.y - m2 * h2.x
+
+  // Intersection:
+  // y = m1*x + b1
+  // y = m2*x + b2
+  // m1*x + b1 = m2*x + b2
+  // x = (b2 - b1)/(m1 - m2)
+  let x0 = (b2 -b1)/(m1 - m2)
+  let y0 = m1 * x0 + b1
+
+  x0 = parseFloat(x0.toFixed(3))
+  y0 = parseFloat(y0.toFixed(3))
+
+  let y1 = m1 * h1.x + b1
+
+
+  if (x0 <= max && x0 >= min && y0 <= max && y0 >= min) {
+    // Check that intersection point is in h1's future
+    if (h1.vx < 0 && x0 > h1.x || h1.vx > 0 && x0 < h1.x) {
+      return false
+    }
+
+    // Check that intersection point is in h2's future
+    if (h2.vx < 0 && x0 > h2.x || h2.vx > 0 && x0 < h2.x) {
+      return false
+    }
+    return true
+  }
+  return false
+}
+
+const countIntersections = (hails: Hail[], min: number, max: number) => {
+  console.log('hail count', hails.length)
+  let count = 0
+  let pairs = 0
+  for(let i = 0; i < hails.length; i++) {
+    for(let j = i + 1; j < hails.length; j++) {
+      pairs++
+
+      // console.log('pair', i, j)
+      hasIntersection(hails[i], hails[j], min, max) ? count++ : undefined
+    }
+  }
+
+  console.log('pairs', pairs)
+  return count
+}
+
+const readHail = () => {
+  let lines = readInput();
+  console.log("lines", lines);
+  let hails = lines.map((l: string) => parseHail(l))
+  console.log('hails', hails)
+
+  let min = 200000000000000
+  let max = 400000000000000
+  let count = countIntersections(hails, min, max)
+
+  console.log('count', count)
+};
+
+readHail();
